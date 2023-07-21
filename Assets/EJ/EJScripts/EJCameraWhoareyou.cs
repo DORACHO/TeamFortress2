2c4338de99702d72fb48bf2952c7809afb51d6c5
DORACHO <135581648+DORACHO@users.noreply.github.com>
2023-07-21 13:45:25 +0900

    07211345은정까지merge

--- Assets/EJ/EJScripts/EJCameraWhoareyou.cs
+++ Assets/EJ/EJScripts/EJCameraWhoareyou.cs
@@ -1,43 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class EJCameraWhoareyou : MonoBehaviour
+namespace XXX
 {
-    // Start is called before the first frame update
-    void Start()
+    public class EJCameraWhoareyou : MonoBehaviour
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Whoareyou();
-    }
-
-    public void Whoareyou()
-    {
-        bool chaseCameraON = GetComponent<EJCameraRotate>().chaseCameraON;
-        bool mainCameraON = GetComponent<EJCameraRotate>().mainCameraON;
-
-        chaseCameraON = true;
-        if (chaseCameraON)
+        // Start is called before the first frame update
+        void Start()
         {
-            print("whoareyou 실행 중입니다");
 
-            Vector3 dir2Enemy = EJPSHP.instance.murdererPos - transform.position;
-            dir2Enemy.Normalize();
+        }
 
-            // transform.parent = null;
-            Vector3 murdererCamPos = EJPSHP.instance.murdererPos + (-dir2Enemy + Vector3.up * 3);          
+        // Update is called once per frame
+        void Update()
+        {
+            Whoareyou();
+        }
 
-            transform.position = Vector3.Lerp(transform.position, murdererCamPos, Time.deltaTime * 5);
+        public void Whoareyou()
+        {
+            bool chaseCameraON = GetComponent<EJCameraRotate>().chaseCameraON;
+            bool mainCameraON = GetComponent<EJCameraRotate>().mainCameraON;
 
-            transform.LookAt(EJPSHP.instance.murdererPos);
-            chaseCameraON = false;
+            chaseCameraON = true;
+            if (chaseCameraON)
+            {
+                print("whoareyou 실행 중입니다");
+
+                Vector3 dir2Enemy = EJPSHP.instance.murdererPos - transform.position;
+                dir2Enemy.Normalize();
+
+                // transform.parent = null;
+                Vector3 murdererCamPos = EJPSHP.instance.murdererPos + (-dir2Enemy + Vector3.up * 3);
+
+                transform.position = Vector3.Lerp(transform.position, murdererCamPos, Time.deltaTime * 5);
+
+                transform.LookAt(EJPSHP.instance.murdererPos);
+                chaseCameraON = false;
+            }
+
         }
 
     }
-
 }
